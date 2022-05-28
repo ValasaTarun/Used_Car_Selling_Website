7a78e378eed17b0cd04bfab36a282b32faa46a77
Tarun <mittuvalasa5@gmail.com>
2022-05-28 20:29:34 +0530

    Added partials for footer and added login route in users.js and added contact page also wrote a route for it in index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Car Selling Website' });
 });
 
+router.get('/contact',(req,res)=>{
+  res.render('contact',{title: 'Contact Page'});
+})
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/login',(req,res)=>{
+  console.log(req.body);
+  res.send('<center><h1> Login Form Submitted </center></h1>');
+})
+
 module.exports = router;
